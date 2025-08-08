1edd7e836146f9f3b9c8a8d414f0856a4d912dec
1-2-2025 <1-2-2025@TAYYABAH>
2025-08-08 22:53:44 +0500

    login form complete

--- RecruitmentSystem/Controllers/AccountsController.cs
+++ RecruitmentSystem/Controllers/AccountsController.cs
@@ -53,5 +53,10 @@ namespace RecruitmentSystem.Controllers
             TempData["Message"] = "Application submitted successfully!";
             return RedirectToAction("Register");
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
