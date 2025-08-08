968b1e01b771521e7fe7d231607e632e0809e0b0
sababintenasim <sababintenasim@gmail.com>
2025-08-08 23:15:05 +0500

    Hr Dashboard

---
+++ RecruitmentSystem/Controllers/HrController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitmentSystem.Controllers
+{
+    public class HrController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Createvacancy()
+        {
+            return View();
+        }
+        public IActionResult viewvacancies()
+        {
+            return View();
+        }
+        public IActionResult ScheduleInterview()
+        {
+            return View();
+        }
+        public IActionResult Adddepartment()
+        {
+            return View();
+        }
+
+    }
+}
