f12e2610bb798ab1b6426a666db1708f1796208d
sababintenasim <sababintenasim@gmail.com>
2025-08-12 20:28:24 +0500

    initial commit All

--- RecruitmentSystem/Controllers/HrController.cs
+++ RecruitmentSystem/Controllers/HrController.cs
@@ -7,14 +7,17 @@ namespace RecruitmentSystem.Controllers
     public class HrController : Controller
     {
         HrdataContext db = new HrdataContext();
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Adddepartment()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Adddepartment(AddDepartment add)
         {
@@ -23,77 +26,94 @@ namespace RecruitmentSystem.Controllers
             return RedirectToAction("Adddepartment");
         }
 
-    
+        // GET: Createvacancy
         public IActionResult Createvacancy()
         {
-            // Fetch departments from DB
             var departments = db.AddDepartments.ToList();
-
-            // ViewBag.adddepartment to hum createvacancy.cshtml me pass karwain gy 
             ViewBag.adddepartment = new SelectList(departments, "DepartmentId", "DepartmentName");
-
             return View();
         }
-       
+
+        // POST: Createvacancy
+        [HttpPost]
+        public IActionResult Createvacancy(Vacancy vc)
+        {
+            db.Vacancies.Add(vc);
+            db.SaveChanges();
+            return RedirectToAction("viewvacancies");
+
+        }
+
         public IActionResult viewvacancies()
         {
             var datashow = db.Vacancies.ToList();
             return View(datashow);
         }
+
         public IActionResult delete(int id)
         {
             var dataitem = db.Vacancies.FirstOrDefault(c => c.VacancyId == id);
             if (dataitem != null)
             {
                 db.Vacancies.Remove(dataitem);
+                db.SaveChanges();
             }
             return RedirectToAction("viewvacancies");
+
+            
         }
+
         public IActionResult editvacancy(int id)
         {
-            var dataitem = db.Vacancies.FirstOrDefault(c =>c.VacancyId == id);
-            ViewBag.adddepartment = new SelectList(db.Vacancies.ToList(), "DepartmentId", "DepartmentName");
+            var dataitem = db.Vacancies.FirstOrDefault(c => c.VacancyId == id);
+            ViewBag.adddepartment = new SelectList(db.AddDepartments.ToList(), "DepartmentId", "DepartmentName");
             return View(dataitem);
         }
+
         [HttpPost]
         public IActionResult editvacancy(Vacancy vac, int id)
         {
-            var existing=  db.Vacancies.Find(id);
+            var existing = db.Vacancies.Find(id);
             if (existing == null)
             {
                 return NotFound();
             }
-            else
-            {
-                existing.JobTitle = vac.JobTitle;
-                existing.DepartmentId = vac.DepartmentId;
-                existing.Country = vac.Country;
-                existing.City = vac.City;
-                existing.JobDescription = vac.JobDescription;
-                existing.ListOfHired = vac.ListOfHired;
-                existing.Owner = vac.Owner;
-                existing.Status = vac.Status;
-                existing.PostedDate = vac.PostedDate;
-                existing.ClosingDate = vac.ClosingDate;
-            }
+
+            existing.JobTitle = vac.JobTitle;
+            existing.DepartmentId = vac.DepartmentId;
+            existing.Country = vac.Country;
+            existing.City = vac.City;
+            existing.JobDescription = vac.JobDescription;
+            existing.ListOfHired = vac.ListOfHired;
+            existing.Owner = vac.Owner;
+            existing.Status = vac.Status;
+            existing.PostedDate = vac.PostedDate;
+            existing.ClosingDate = vac.ClosingDate;
+
             db.Vacancies.Update(existing);
             db.SaveChanges();
-            ViewBag.adddepartment = new SelectList(db.Vacancies.ToList(), "DepartmentId", "DepartmentName");
-            return View();
+
+            return RedirectToAction("viewvacancies");
         }
 
         public IActionResult ScheduleInterview()
         {
             return View();
         }
+
         public IActionResult Search()
         {
             return View();
         }
+
         public IActionResult Hrprofile()
         {
             return View();
         }
 
+        public IActionResult ApplicantRq()
+        {
+            return View();
+        }
     }
 }
